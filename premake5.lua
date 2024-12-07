a4bf766742d040cacf70d68a14b3618e1aa72611
Ema Favaro <emanuel@favaro.uy>
2024-12-07 00:04:49 -0300

    Update premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,63 @@
+workspace "Heavenly Polygon"
+    architecture "x64"
+    startproject "OpenGL_Project"
+
+    configurations { "Debug", "Release" }
+
+outputdir = "%{cfg.buildcfg}/%{cfg.system}-%{cfg.architecture}"
+
+project "Heavenly Polygon"
+    location "."
+    kind "ConsoleApp"  -- Console-based application
+
+    language "C"
+    cdialect "C99"
+
+    staticruntime "on" -- Statically link runtime library (makes executable self-contained)
+
+    targetdir ("bin/" .. outputdir)
+    objdir ("bin/" .. outputdir .. "/obj_output")
+
+    files {
+        "src/**.c",
+        "src/**.h",
+        "third-party/glad.c",
+    }
+
+    includedirs {
+        "src",
+        "third-party/include"
+    }
+
+    libdirs {
+        "third-party/lib"
+    }
+
+    links {
+        "glfw3.lib",       -- Link GLFW static library
+        "opengl32.lib"     -- Link OpenGL (default Windows OpenGL library)
+    }
+
+        -- Organize Filters (Solution Explorer in Visual Studio)
+    vpaths {
+        ["Source Files/*"] = { "src/**.c" },
+        ["Source Files/third-party*"] = { "third-party/*.c" },
+        ["Header Files/*"] = { "src/**.h" },
+        ["Header Files/third-party/*"] = { "third-party/include/**.h" },
+        ["Resource Files/Shaders"] = {  "resources/shaders/**.vert", "resources/shaders/**.frag"  }
+    }
+
+
+    filter "system:windows"
+        systemversion "latest" -- Use latest Windows SDK
+
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        runtime "Release"
+        optimize "on"
